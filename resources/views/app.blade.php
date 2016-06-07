44f2c960ced76d9ea392139ddcef13ca9b0717d3
Walber Jefferson <walberjefferson@hotmail.com>
2016-06-07 16:32:18 -0300

    teste

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Code Delivery</title>
 
-	<!--<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">-->
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 	<link href="/css/bootstrap.min.css" rel="stylesheet">
 
 	<!-- Fonts -->
